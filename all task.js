ff69cf5b04ea74cc6a2417be4d57b437ffb81413
Foysal-Munsy <bdfoysal555@gmail.com>
2025-02-18 01:32:03 +0600

    task 4

--- all task.js
+++ all task.js
@@ -34,3 +34,10 @@ function electionResult(votes) {
   }
   return mango === banana ? "Draw" : mango > banana ? "Mango" : "Banana";
 }
+
+function isBestFriend(f1, f2) {
+  if (typeof f1 !== "object" || typeof f2 !== "object") return "Invalid";
+
+  if (f1.bestFriend === f2.roll && f2.bestFriend === f1.roll) return true;
+  return false;
+}
